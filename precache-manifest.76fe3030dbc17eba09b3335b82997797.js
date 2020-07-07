7af2c1e00ba19a2784709d8f655113b3e05911d3
im-adithya <imadithyavardhan@gmail.com>
2020-07-07 13:12:42 +0530

    Updates

---
+++ precache-manifest.76fe3030dbc17eba09b3335b82997797.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f81e093daee05b106fe718ad79e41341",
+    "url": "/To-Do-List/index.html"
+  },
+  {
+    "revision": "689720b965da70465ae5",
+    "url": "/To-Do-List/static/js/2.a673be8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-List/static/js/2.a673be8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee692dea238867bd37b2",
+    "url": "/To-Do-List/static/js/main.049d1c54.chunk.js"
+  },
+  {
+    "revision": "e71b0fca7b414be357cd",
+    "url": "/To-Do-List/static/js/runtime-main.8e2d492d.js"
+  }
+]);
